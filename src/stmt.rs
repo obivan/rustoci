7204afbd6130338c93d278980a277c9ea33f8acb
Vadim Komarov <dotvad@gmail.com>
2014-12-08 00:20:45 +0500

    Implemented Statement.prepare

--- src/stmt.rs
+++ src/stmt.rs
@@ -13,4 +13,13 @@ impl Statement {
         ) as *mut ffi::OCIStmt;
         Ok(Statement {conn: conn, stmt_handle: stmt_handle})
     }
+
+    pub fn prepare(self, stmt_text: String) -> Result<Statement, ffi::OracleError> {
+        let stmt_handle = try!(
+            ffi::oci_stmt_prepare2(self.conn.service_handle, self.conn.env.error_handle,
+                                   stmt_text, None)
+        );
+        Ok(Statement {conn: self.conn, stmt_handle: stmt_handle})
+    }
+
 }
