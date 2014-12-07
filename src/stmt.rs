7898d87e3bbd077a6ac6ab92a3bfd2f71c4bef21
Vadim Komarov <dotvad@gmail.com>
2014-12-08 00:44:43 +0500

    Statement.prepare -> Statement.new

--- src/stmt.rs
+++ src/stmt.rs
@@ -7,24 +7,17 @@ pub struct Statement {
 }
 
 impl Statement {
-    pub fn new(conn: conn::Connection) -> Result<Statement, ffi::OracleError> {
+    pub fn new(conn: conn::Connection, stmt_text: String) -> Result<Statement, ffi::OracleError> {
         let stmt_handle = try!(
-            ffi::oci_handle_alloc(conn.env.handle, ffi::OCIHandleType::Statement)
-        ) as *mut ffi::OCIStmt;
-        Ok(Statement {conn: conn, stmt_handle: stmt_handle})
-    }
-
-    pub fn prepare(self, stmt_text: String) -> Result<Statement, ffi::OracleError> {
-        let stmt_handle = try!(
-            ffi::oci_stmt_prepare2(self.conn.service_handle, self.conn.env.error_handle,
-                                   stmt_text, None)
+            ffi::oci_stmt_prepare2(conn.service_handle, conn.env.error_handle, stmt_text, None)
         );
-        Ok(Statement {conn: self.conn, stmt_handle: stmt_handle})
+        Ok(Statement {conn: conn, stmt_handle: stmt_handle})
     }
 
     pub fn execute(self) -> Result<Statement, ffi::OracleError> {
         try!(
-            ffi::oci_stmt_execute(self.conn.service_handle, self.stmt_handle,
+            ffi::oci_stmt_execute(self.conn.service_handle,
+                                  self.stmt_handle,
                                   self.conn.env.error_handle)
         );
         Ok(self)
